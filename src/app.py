c588d4376dc5207f95acd75c78e00f779e99235c
dustin-jacobsen-colorado-edu <dustin.jacobsen@colorado.edu>
2024-06-11 11:24:28 -0600

    Modified page text

--- src/app.py
+++ src/app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 def main():
     return '''
      <form action="/echo_user_input" method="POST">
+	 <p>Please enter some text. After clicking the Submit button, your text will be displayed on this page."</p>
          <input name="user_input">
-         <input type="submit" value="Submit!">
+         <input type="submit" value="Submit">
      </form>
      '''
 
